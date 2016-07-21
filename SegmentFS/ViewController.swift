53133f8c163675d57b0f60e7f857d35cfe2a927a
David Wagner <mnem@noiseandheat.com>
2016-07-21 21:31:13 +0100

    Initial commit.

---
+++ SegmentFS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SegmentFS
+//
+//  Created by David Wagner on 21/07/2016.
+//  Copyright © 2016 Noise & Heat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
